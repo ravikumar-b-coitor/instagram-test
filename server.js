4337cdb08bdccd24fa16ace2c09206b123acdfdc
ravikumarbalasubramanian11 <ravikumar.balasubramanian11@gmail.com>
2024-07-24 17:55:27 +0530

    io

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(cors()); // Enable CORS for all origins
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-	console.log(res);
-	res.send('Hello World!');
+  console.log(res);
+  res.send('Hello World!');
 });
 
-app.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
