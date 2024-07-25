5625867f92ed3d16975a554ce0e382d050814251
ravikumarbalasubramanian11 <ravikumar.balasubramanian11@gmail.com>
2024-07-25 12:58:14 +0530

    io

--- server.js
+++ server.js
@@ -9,10 +9,53 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  console.log(res);
-  res.send('Hello World!');
+	res.send('Hello World!');
+});
+
+app.get('/insta', (req, res) => {
+	console.log(`
+		
+		`);
+	console.log("GET   ---   Instagram => ", 'Params:', req.params, 'Query:', req.query);
+	console.log('Body:', JSON.stringify(req.body));
+
+	const challenge = req.query['hub.challenge'];
+
+	if (challenge) {
+		res.send(challenge);
+	}
+});
+
+app.post('/insta', (req, res) => {
+	console.log("POST   ---   Instagram => ", 'Params:', req.params, 'Query:', req.query);
+	console.log('Body:', JSON.stringify(req.body));
+	// Handle webhook events here
+
+	res.status(200).send('Event received');
+});
+
+app.get('/user', (req, res) => {
+	console.log(`
+		
+	`);
+	console.log("GET   ---   User => ", 'Params:', req.params, 'Query:', req.query);
+	console.log('Body:', JSON.stringify(req.body));
+
+	const challenge = req.query['hub.challenge'];
+
+	if (challenge) {
+		res.send(challenge);
+	}
+});
+
+app.post('/user', (req, res) => {
+	console.log("POST   -----   User => ", 'Params:', req.params, 'Query:', req.query);
+	console.log('Body:', JSON.stringify(req.body));
+	// Handle webhook events here
+
+	res.status(200).send('Event received');
 });
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running at http://0.0.0.0:${port}`);
+	console.log(`Server running at http://0.0.0.0:${port}`);
 });
