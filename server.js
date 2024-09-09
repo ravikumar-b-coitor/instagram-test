7775b5fe51fcf0e50026f9705083962c914a222d
ravikumarbalasubramanian11 <ravikumar.balasubramanian11@gmail.com>
2024-09-09 11:20:19 +0530

    Updated

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const API_URL = "https://api-digitalwall.coitor.com/"
+// const API_URL = "https://api-digitalwall.coitor.com/"
+const API_URL = "https://api-digitalwall.xploro.io/"
 
 const server = createServer(app);
 const io = new Server(server, {
