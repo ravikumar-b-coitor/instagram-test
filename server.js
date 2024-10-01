e6400d0861911e809bfcd95f1a3750b5ada66e56
ravikumarbalasubramanian11 <ravikumar.balasubramanian11@gmail.com>
2024-10-01 22:56:19 +0530

    io

--- server.js
+++ server.js
@@ -304,8 +304,8 @@ app.post('/insta', async (req, res) => {
 
 			const API_URLS = [
 				"https://api-digitalwall.coitor.com/Instagram/AddInstaDm/",
-				"https://api-digitalwall.xploro.io/Instagram/AddInstaDm/",
-				"https://api-digitalwall-demo.xploro.io/Instagram/AddInstaDm/"
+				// "https://api-digitalwall.xploro.io/Instagram/AddInstaDm/",
+				// "https://api-digitalwall-demo.xploro.io/Instagram/AddInstaDm/"
 			];
 
 			Promise.allSettled(
