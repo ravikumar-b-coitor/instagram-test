4a9b17433bd5c41992e81e551e37491f0942c418
ravikumarbalasubramanian11 <ravikumar.balasubramanian11@gmail.com>
2024-10-01 17:47:31 +0530

    io

--- server.js
+++ server.js
@@ -275,7 +275,7 @@ app.post('/insta', async (req, res) => {
 					if (result.status === 'fulfilled') {
 						console.log(`Success response from ${API_URLS[index]}:`, result.value.data);
 					} else {
-						console.error(`Error response from ${API_URLS[index]}:`, result.reason.message);
+						console.error(`Error response from ${API_URLS[index]}:`, result);
 					}
 				});
 
