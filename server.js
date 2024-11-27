feed3360224e8bcd1fc3aafdfe80981eed9dd2bf
ravikumarbalasubramanian11 <ravikumar.balasubramanian11@gmail.com>
2024-11-27 11:39:20 +0530

    io

--- server.js
+++ server.js
@@ -57,13 +57,22 @@ io.on('connection', (socket) => {
 });
 
 app.get('/insta', (req, res) => {
-	console.log("GET   ---   Instagram => ", 'Params:', req.params, 'Query:', req.query);
-	console.log('Body:', JSON.stringify(req.body));
+	// Parse the query params
+	let mode = req.query["hub.mode"];
+	let token = req.query["hub.verify_token"];
+	let challenge = req.query["hub.challenge"];
 
-	const challenge = req.query['hub.challenge'];
-
-	if (challenge) {
-		res.send(challenge);
+	// Check if a token and mode is in the query string of the request
+	if (mode && token) {
+		// Check the mode and token sent is correct
+		if (mode === "subscribe" && token === config.verifyToken) {
+			// Respond with the challenge token from the request
+			console.log("WEBHOOK_VERIFIED");
+			res.status(200).send(challenge);
+		} else {
+			// Respond with '403 Forbidden' if verify tokens do not match
+			res.sendStatus(403);
+		}
 	}
 });
 
