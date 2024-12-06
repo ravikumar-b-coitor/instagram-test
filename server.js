28008fcb62376e36bd033636610e103bda5e6d37
ravikumarbalasubramanian11 <ravikumar.balasubramanian11@gmail.com>
2024-12-06 12:32:43 +0530

    add

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
