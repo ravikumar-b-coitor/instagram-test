0601294534fb11808376f1ef8787a6ee7d23beb8
ravikumarbalasubramanian11 <ravikumar.balasubramanian11@gmail.com>
2025-03-10 13:00:35 +0530

    io

--- audio.js
+++ audio.js
@@ -1,12 +1,12 @@
 "use strict";
 
+const fetch = require("node-fetch").default;
 const ffmpeg = require("fluent-ffmpeg");
+const FormData = require("form-data");
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const fetch = require("node-fetch").default;
-const FormData = require("form-data");
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() }); // Store in memory, not disk
 
@@ -15,7 +15,7 @@ app.post("/process", upload.single("Recording"), async (req, res) => {
 		if (!req.file) return res.status(400).send("No audio file uploaded");
 		console.time("Processing Time");
 
-		const {url, StaffId, StartTime, EndTime } = req.body;
+		const {Url, StaffId, StartTime, EndTime } = req.body;
 
 		// Determine input file format
 		const inputFormat = req.file.mimetype.split("/")[1];
@@ -51,10 +51,10 @@ app.post("/process", upload.single("Recording"), async (req, res) => {
 
 				// Send the processed audio via fetch
 				try {
-					const response = await fetch(`${url}Register/CreateCallRecording`, {
+					const response = await fetch(`${Url}Register/CreateCallRecording`, {
 						method: "POST",
 						headers: {
-							Authorization: `Bearer ${req.headers.authorization}`,
+							Authorization: `Bearer ${req?.headers?.authorization}`,
 						},
 						body: formData,
 					});
